c4dc9d23f76ad69b7d02ba9b84ee411284a2f04b
Genre Mamanao <41022228+glenngenre@users.noreply.github.com>
2025-08-25 22:27:39 +0800

    fix: update password regex for improved validation

--- src/main/java/me/faun/authentication/util/ValidationUtils.java
+++ src/main/java/me/faun/authentication/util/ValidationUtils.java
@@ -2,7 +2,7 @@ package me.faun.authentication.util;
 
 public class ValidationUtils {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)$";
-    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
 
     public static boolean isValidEmail(String email) {
         return email != null && email.matches(EMAIL_REGEX);
@@ -14,13 +14,13 @@ public class ValidationUtils {
 
     public static String getPasswordRequirements() {
         return """
-               Password must:
-               - Be at least 8 characters long
-               - Contain at least one digit
-               - Contain at least one lowercase letter
-               - Contain at least one uppercase letter
-               - Contain at least one special character (@#$%^&+=)
-               - No whitespace allowed
-               """;
+                Password must:
+                - Be at least 8 characters long
+                - Contain at least one digit
+                - Contain at least one lowercase letter
+                - Contain at least one uppercase letter
+                - Contain at least one special character (@#$%^&+=)
+                - No whitespace allowed
+                """;
     }
 }
