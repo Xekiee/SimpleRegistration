f6d617d23a4c31230844b5212d7d278510877c5e
Genre Mamanao <41022228+glenngenre@users.noreply.github.com>
2025-08-25 16:34:12 +0800

    Code cleanup

--- src/test/java/me/faun/authentication/AuthServiceTest.java
+++ src/test/java/me/faun/authentication/AuthServiceTest.java
@@ -2,8 +2,6 @@ package me.faun.authentication;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AuthServiceTest {
 
     @Test
--- src/test/java/me/faun/authentication/util/ValidationUtilsTest.java
+++ src/test/java/me/faun/authentication/util/ValidationUtilsTest.java
@@ -2,8 +2,6 @@ package me.faun.authentication.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ValidationUtilsTest {
 
     @Test
